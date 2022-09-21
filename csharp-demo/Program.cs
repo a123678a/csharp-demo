873ccb8e58ab3b30179e9f2b1cf43b10813847b0
a123678a <tlstjddud14@gmail.com>
2022-09-21 23:28:35 +0900

    초기 커밋.

---
+++ csharp-demo/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Windows;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CsharpProject
+{
+    class Program
+    {
+        [STAThread]
+        static void Main(string[] args)
+        {
+            Application app = new Application();
+            app.StartupUri = new Uri("MainWindow.xaml", UriKind.Relative);
+            app.Run();
+        }
+    }
+}
